daf36a05a52ec0651343acbb900bbc0924134556
Fdasz <147099586+Fdasz@users.noreply.github.com>
2024-06-02 15:19:10 -0400

    Update cliente.cpp

--- cliente.cpp
+++ cliente.cpp
@@ -1,54 +1,68 @@
-#include <iostream>
-#include <cstring>
+#include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
 
-void printBoard(char board[6][7]) {
-    for(int i = 0; i < 6; ++i) {
-        for(int j = 0; j < 7; ++j) {
-            std::cout << board[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
-}
+using namespace std;
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char *argv[]) {
     if (argc != 3) {
-        std::cerr << "Uso: " << argv[0] << " <direccion_ip> <puerto>\n";
+        cerr << "Uso: " << argv[0] << " <direccion IP> <puerto>" << endl;
         return 1;
     }
 
-    const char* server_ip = argv[1];
+    const char *server_ip = argv[1];
     int port = atoi(argv[2]);
-
+    int sock = 0;
     struct sockaddr_in serv_addr;
-    int sock = 0, valread;
-    char buffer[1024] = {0};
-    char board[6][7];
 
+    // Crear el socket
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        std::cout << "\nError al crear el socket\n";
-        return -1;
+        cerr << "Error creando el socket: " << strerror(errno) << endl;
+        return 1;
     }
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(port);
 
+    // Convertir la dirección IP
     if (inet_pton(AF_INET, server_ip, &serv_addr.sin_addr) <= 0) {
-        std::cout << "\nDirección IP inválida o no soportada\n";
-        return -1;
+        cerr << "Dirección IP inválida o no soportada: " << server_ip << endl;
+        return 1;
     }
 
+    // Conectar al servidor
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        std::cout << "\nConexión fallida\n";
-        return -1;
+        cerr << "Conexión fallida: " << strerror(errno) << endl;
+        return 1;
     }
 
-    while (true) {
-        valread = read(sock, board, sizeof(board));
-        printBoard(board);
+    cout << "Conectado al servidor " << server_ip << " en el puerto " << port << endl;
 
-        std::cout << "Ingrese columna (0-6): ";
-        int col;
-        std::cin >> col;
-        send(sock, std::to_string(col).c_str(), sizeof(std::to_string(col).c_str()), 0
+    char buffer[1024] = {0};
+    while (true) {
+        // Recibir mensaje del servidor
+        int valread = read(sock, buffer, 1024);
+        if (valread <= 0) {
+            cerr << "Conexión cerrada por el servidor o error al recibir datos" << endl;
+            break;
+        }
+
+        cout << buffer << endl;
+
+        // Enviar columna elegida al servidor
+        if (strstr(buffer, "Elija una columna (1-7):")) {
+            string input;
+            cout << "Ingrese la columna: ";
+            cin >> input;
+            send(sock, input.c_str(), input.length(), 0);
+        }
+
+        memset(buffer, 0, sizeof(buffer));
+    }
+
+    close(sock);
+    return 0;
+}
